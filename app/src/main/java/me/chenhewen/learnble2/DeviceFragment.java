38f5ac2333393404c400c72ecfbb33b345e3bbc7
陈鹤文 <532531210@qq.com>
2024-10-14 17:12:51 +0800

    增加与编辑界面

--- app/src/main/java/me/chenhewen/learnble2/DeviceFragment.java
+++ app/src/main/java/me/chenhewen/learnble2/DeviceFragment.java
@@ -12,20 +12,25 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import me.chenhewen.learnble2.model.InfoItem;
+
 public class DeviceFragment extends Fragment {
 
-    private List<InfoItem> mockItems = new ArrayList<>(Arrays.asList(
-        new InfoItem("Hello"),
-        new InfoItem("World")
-    ));
-    private List<InfoItem> infoItems = mockItems;
+    private List<InfoItem> infoItems = InfoItem.mockItems;
+
+    private String[] uuidMockItems = new String[] {"A-A-A-A", "B-B-B-B", "C-C-C-C", "D-D-D-D"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,6 +40,15 @@ public class DeviceFragment extends Fragment {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(new DeviceFragment.MyRecyclerAdapter());
+
+        View fabButton = contentView.findViewById(R.id.fab);
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSheet(null);
+            }
+        });
+
         return contentView;
     }
 
@@ -49,9 +63,18 @@ public class DeviceFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull DeviceFragment.MyViewHolder holder, int position) {
+            paddingLastItem(holder, position, infoItems.size());
             InfoItem infoItem = infoItems.get(position);
-            holder.dataStringView.setText(infoItem.sendString);
-            holder.dataHexView.setText(infoItem.getSendHexString());
+            holder.titlView.setText(infoItem.title);
+            if (infoItem.sendDataType == InfoItem.SendDataType.STRING) {
+                holder.dataStringView.setVisibility(View.VISIBLE);
+                holder.dataStringView.setText(infoItem.sendString);
+                holder.dataHexView.setText(infoItem.getDisplayHexString());
+            } else if (infoItem.sendDataType == InfoItem.SendDataType.HEX) {
+                holder.dataStringView.setVisibility(View.GONE);
+            }
+            holder.dataHexView.setText(infoItem.getDisplayHexString());
+
             holder.moreButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -67,6 +90,7 @@ public class DeviceFragment extends Fragment {
                                 // TODO:
                             } else if (item.getItemId() == R.id.action_edit) {
                                 // TODO:
+                                openSheet(infoItem);
                             } else if (item.getItemId() == R.id.action_delete) {
                                 // TODO:
                             }
@@ -85,47 +109,83 @@ public class DeviceFragment extends Fragment {
         public int getItemCount() {
             return infoItems.size();
         }
+
+        private void paddingLastItem(MyViewHolder holder, int position, int listSize) {
+            if (position == listSize - 1){
+                RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) holder.itemView.getLayoutParams();
+                layoutParams.bottomMargin = 500;
+                holder.itemView.setLayoutParams(layoutParams);
+            } else{
+                RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) holder.itemView.getLayoutParams();
+                layoutParams.bottomMargin = 0;
+                holder.itemView.setLayoutParams(layoutParams);
+            }
+        }
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView titlView;
         TextView dataStringView;
         TextView dataHexView;
         ImageButton moreButton;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            titlView = itemView.findViewById(R.id.item_title);
             dataStringView = itemView.findViewById(R.id.item_send_string);
             dataHexView = itemView.findViewById(R.id.item_send_hex);
             moreButton = itemView.findViewById(R.id.item_more_button);
         }
     }
 
-    public static class InfoItem {
-        public InfoItem(String sendString) {
-            this.sendString = sendString;
+    private void openSheet(InfoItem infoItem) {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext());
+        View sheetContentView = LayoutInflater.from(getContext()).inflate(R.layout.device_add_sheet, null);
+        bottomSheetDialog.setContentView(sheetContentView);
+        bottomSheetDialog.show();
+
+        InfoItem mockInfoItem = infoItems.get(1);
+        if (infoItem == null) {
+            infoItem = mockInfoItem;
         }
 
-        public String sendString;
+        // UUID
+        String[] uuidItems = uuidMockItems;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_dropdown_item_1line, uuidItems);
+        AutoCompleteTextView uuidSelectionView = sheetContentView.findViewById(R.id.uuid_selection_view);
+        uuidSelectionView.setAdapter(adapter);
+        uuidSelectionView.setText(infoItem.uuid, false);
 
-        public String getSendHexString() {
-            StringBuilder hexString = new StringBuilder("0x: ");
+        // Title
+        TextInputEditText titleView = sheetContentView.findViewById(R.id.title_view);
+        titleView.setText(infoItem.title);
 
-            for (int i = 0; i < sendString.length(); i++) {
-                // 获取字符的16进制表示
-                String hex = Integer.toHexString(sendString.charAt(i));
-                // 保证每个字符占两个字节，不足的补0
-                if (hex.length() < 2) {
-                    hexString.append("0");
-                }
-                hexString.append(hex);
+        // Data type
+        String[] dataTypeItems = InfoItem.SendDataType.getAllValues();
+        ArrayAdapter<String> dataTypeAdapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_dropdown_item_1line, dataTypeItems);
+        AutoCompleteTextView dataTypeSelectionView = sheetContentView.findViewById(R.id.data_type_selection);
+        dataTypeSelectionView.setAdapter(dataTypeAdapter);
+        dataTypeSelectionView.setText(infoItem.sendDataType.getRawValue(), false);
 
-                // 如果不是最后一个字符，添加逗号
-                if (i < sendString.length() - 1) {
-                    hexString.append(", ");
-                }
+        // Send message
+        TextInputEditText msgView = sheetContentView.findViewById(R.id.msg_view);
+        if (infoItem.sendDataType == InfoItem.SendDataType.STRING) {
+            msgView.setText(infoItem.sendString);
+        } else if (infoItem.sendDataType == InfoItem.SendDataType.HEX) {
+            msgView.setText(InfoItem.convertIntArrayToHexString(infoItem.sendHex));
+        }
+
+        // Actions
+        View sendButton = sheetContentView.findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = titleView.getText().toString();
+                System.out.println("title: " + title);
+                bottomSheetDialog.dismiss();
             }
-
-            return hexString.toString();
-        }
+        });
     }
 }
---
+++ app/src/main/java/me/chenhewen/learnble2/model/InfoItem.java
@@ -0,0 +1,87 @@
+package me.chenhewen.learnble2.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InfoItem {
+
+    public InfoItem(String uuid, String title, SendDataType sendDataType, String sendString, int[] sendHex) {
+        this.uuid = uuid;
+        this.title = title;
+        this.sendDataType = sendDataType;
+        this.sendString = sendString;
+        this.sendHex = sendHex;
+    }
+
+    public String uuid;
+    public String title;
+    public SendDataType sendDataType;
+    public String sendString;
+    public int[] sendHex;
+
+    public String getDisplayHexString() {
+        if (sendDataType == SendDataType.STRING) {
+            return "0x: " + convertStringToHexString(sendString);
+        } else if (sendDataType == SendDataType.HEX) {
+            return "0x: " + convertIntArrayToHexString(sendHex);
+        }
+
+        return "";
+    }
+
+    public static String convertStringToHexString(String str) {
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            String hex = Integer.toHexString(str.charAt(i));
+            if (hex.length() < 2) {
+                hexString.append("0");
+            }
+            hexString.append(hex);
+            if (i < str.length() - 1) {
+                hexString.append(", ");
+            }
+        }
+
+        return hexString.toString();
+    }
+
+    public static String convertIntArrayToHexString(int[] array) {
+        StringBuilder hexString = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            hexString.append(String.format("%02x", array[i]));
+            if (i < array.length - 1) {
+                hexString.append(", ");
+            }
+        }
+
+        return hexString.toString();
+    }
+
+    public enum SendDataType {
+        STRING("String"),
+        HEX("Hex");
+
+        private final String rawValue;
+
+        // 构造函数
+        SendDataType(String rawValue) {
+            this.rawValue = rawValue;
+        }
+
+        // 自定义方法，返回自定义的字符串
+        public String getRawValue() {
+            return rawValue;
+        }
+
+        public static String[] getAllValues() {
+            return new String[] {"String", "Hex"};
+        }
+    }
+
+    public static List<InfoItem> mockItems = new ArrayList<>(Arrays.asList(
+            new InfoItem("AA-BB-CC-DD", "Hello", InfoItem.SendDataType.STRING, "hello", null),
+            new InfoItem("BB-CC-DD-EE", "World", InfoItem.SendDataType.HEX, null, new int[] {0x57, 0x6f, 0x72, 0x6c, 0x64})
+    ));
+}
